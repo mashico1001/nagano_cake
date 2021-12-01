ebad630d052df76a2d6c2f80798b0fd796c9ebc1
mashico1001 <wabumpr@gmail.com>
2021-12-01 17:19:52 +0000

    [Add]管理者ログイン、トップコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  
+
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+    passwords: 'admin/passwords'
+  }
+  
+  namespace :admin do
+    get '/' => "homes#top"
+  end
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
