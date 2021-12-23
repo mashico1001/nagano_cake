3aa6bfae9b22cc81f6615da56a898dd760b4b29a
mashico1001 <wabumpr@gmail.com>
2021-12-23 17:05:56 +0000

    [Add]会員ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,28 @@
 Rails.application.routes.draw do
   
-
-  devise_for :customers, module: "public"
+  scope module: :public do
+    devise_for :customers, controllers: {
+      sessions: 'public/customers/sessions',
+      passwords: 'public/customers/passwords',
+      registrations: 'public/customers/registrations'
+    }
+    get '/' => "homes#top"
+    get '/about' => "homes#about"
+  end
   
   devise_for :admin, controllers: {
     sessions: 'admin/sessions',
     passwords: 'admin/passwords'
   }
+
   
   namespace :admin do
     get '/' => "homes#top"
     
+    
     resources :items, except: [:destroy]
     resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
   
     
   end
