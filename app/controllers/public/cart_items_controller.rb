cf4e290c2de03c6f799267ff8048a23124b2868a
mashico1001 <wabumpr@gmail.com>
2022-01-16 10:17:25 +0000

    [Add]カート内実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,6 +7,15 @@ class Public::CartItemsController < ApplicationController
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
+    @cart_items = current_customer.cart_items.all
+      @cart_items.each do |cart_item|
+        if cart_item.item_id == @cart_item.item_id
+          new_amount = cart_item.amount.to_i + @cart_item.amount.to_i
+          cart_item.update_attribute(:amount, new_amount)
+          @cart_item.delete
+        end
+      end
+      
     @cart_item.save
     redirect_to cart_items_path
   end
@@ -15,9 +24,14 @@ class Public::CartItemsController < ApplicationController
   end
   
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
   end
   
   def destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to cart_items_path
     
   end
   
